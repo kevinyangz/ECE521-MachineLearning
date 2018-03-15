06088fc8028370e409aad0b19feb5696855d1791
kevinyangz <kevinze.yang@mail.utoronto.ca>
2018-03-15 19:14:14 -0400

    add accuracy

--- Assignment 2/part2.2.1.py
+++ Assignment 2/part2.2.1.py
@@ -27,10 +27,10 @@ def Build_Graph():
     crossEntropyLoss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_target,logits=y_predicted))+ tf.divide(0.01,2)*tf.nn.l2_loss(W)
     optimizer = tf.train.AdamOptimizer(learning_rate = learn_rate)
     train = optimizer.minimize(loss=crossEntropyLoss)
-    return crossEntropyLoss,W,b,X,y_target,train,learn_rate
+    return crossEntropyLoss,W,b,X,y_target,train,learn_rate,y_predicted
 
 trainMinstData,trainMinstTarget,validMinstData,validMinstTarget,testMinstData,testMinstTarget= Load_NotMinst()
-loss,W,B,X,y_target,train,learning_rate= Build_Graph()
+loss,W,B,X,y_target,train,learning_rate,y_predicted_label= Build_Graph()
 
 init = tf.global_variables_initializer()
 sess = tf.InteractiveSession()
@@ -50,19 +50,27 @@ num_epochs = int(iterations/30) #15000 Train Sample
 
 result=[]
 learn_rate=[0.005,0.001,0.0001]
+best_Y_Predicted=[]
 for learn in learn_rate:
     tempresult=[]
+    weight=[]
+    bias=[]
     for step in range(0,num_epochs):
             for i in range(0,30):
                 start_index = i* 500
                 minix=trainMinstData[start_index:start_index+500]
                 miniy=one_hot_trainMinstTarget[start_index:start_index+500]
 
-                err,train_r,weight,bias=sess.run([loss,train,W,B],feed_dict={X:minix,y_target:miniy,learning_rate:learn})
+                err,train_r,weight,bias,y_predicted=sess.run([loss,train,W,B,y_predicted_label],feed_dict={X:minix,y_target:miniy,learning_rate:learn})
                 #print(err)
                 #_, err, currentW, currentb, yhat = sess.run([train, cross_entropy_loss, w, b, y_predicted], feed_dict={x: minix, y_target: miniy,learn_rate:learnrate, weight_decay:weightdecay})
 
             tempresult.append(err)
+    
+    y_predic =sess.run(tf.nn.softmax(sess.run(y_predicted_label,feed_dict={X:trainMinstData})))
+    prediction_accuracy=tf.equal(tf.argmax(y_predic,1),tf.argmax(one_hot_trainMinstTarget,1))
+    accur=sess.run(tf.reduce_mean(tf.cast(prediction_accuracy,tf.float32)))
+    best_Y_Predicted.append(accur)
     result.append(tempresult)
     sess.run(init)
 
