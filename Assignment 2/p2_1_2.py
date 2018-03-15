16db0f07379a9b20c79b9d01351583d00d5fa5da
Amy Z <wen_ying1779@yahoo.ca>
2018-03-14 23:49:03 -0400

    part 2.1.2 adam optimizer vs sgd

--- Assignment 2/p2_1_2.py
+++ Assignment 2/p2_1_2.py
@@ -26,6 +26,24 @@ def Load_data():
         #trainData is a tuple of size 3500
         return trainData,trainTarget,validData,validTarget,testData,testTarget
 
+def build_graph_adam():
+    W = tf.Variable(tf.truncated_normal(shape=[784,1], stddev=0.5), name='weights_adam') #not too sure about stddev
+    b = tf.Variable(0.0, name='biases_adam')
+    X = tf.placeholder(tf.float32, [None, 784], name='input_x_adam')
+    y_target = tf.placeholder(tf.float32, [None,1], name='target_y_adam')
+    learn_rate=tf.placeholder(tf.float32,shape=[],name='learn_rate_adam')
+    weight_decay = tf.placeholder(tf.float32,shape=[],name='weight_decay_adam')
+    
+    # Graph definition
+    y_predicted = tf.matmul(X,W) + b
+        
+    #crossEntropyLoss = tf.reduce_sum(-y_target*log(y_predicted)-(1-y_target)*log(1-y_predicted), 1)  + tf.divide(weight_decay,2)*tf.squeeze(tf.matmul(W,W,transpose_a=True))
+    crossEntropyLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y_target,logits=y_predicted))+ tf.divide(weight_decay,2)*tf.squeeze(tf.matmul(W,W,transpose_a=True))
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate = learn_rate)
+    train = optimizer.minimize(loss=crossEntropyLoss)
+    return W, b, X, y_target, y_predicted,learn_rate,weight_decay,crossEntropyLoss, train
+
 def build_graph():
         W = tf.Variable(tf.truncated_normal(shape=[784,1], stddev=0.5), name='weights') #not too sure about stddev
         b = tf.Variable(0.0, name='biases')
@@ -47,52 +65,49 @@ def build_graph():
 
 trainData,trainTarget,validData,validTarget,testData,testTarget=Load_data()
 w,b,x,y_target,y_predicted,learn_rate,weight_decay,cross_entropy_loss,train=build_graph()
+w_adam,b_adam,x_adam,y_target_adam,y_predicted_adam,learn_rate_adam,weight_decay_adam,cross_entropy_loss_adam,train_adam=build_graph_adam()
 
 #Reshape the data to N x 784 format
 N=len(trainData)
 trainData = np.reshape(trainData, [N, 28*28])
-#validData = np.reshape(validData, [len(validData), 28*28])
-#print(trainTarget)
 init = tf.global_variables_initializer()
 sess = tf.InteractiveSession()
 sess.run(init)
-learn= [0.005,0.001,0.0001]
 weightdecay = 0.01
 result=[]
 weight_list = []
 bias_list = []
-#valid_mse_list = []
 epochs=0
 iterations = 5000
 num_epochs = int(iterations/7)
-for rate in learn:
-    #w,b,x,y_target,y_predicted,learn_rate,mse,train=build_graph()
-    learnrate=rate
+
+for optimizer in range(0,2):
+    learnrate=0.001
     tempresult=[]
     for step in range(0,num_epochs):
         for i in range(0,7):
             start_index = i* 500
             minix=trainData[start_index:start_index+500]
             miniy=trainTarget[start_index:start_index+500]
-            _, err, currentW, currentb, yhat = sess.run([train, cross_entropy_loss, w, b, y_predicted], feed_dict={x: minix, y_target: miniy,learn_rate:learnrate, weight_decay:weightdecay})
+            if(optimizer == 0):
+                _, err, currentW, currentb, yhat = sess.run([train, cross_entropy_loss, w, b, y_predicted], feed_dict={x: minix, y_target: miniy,learn_rate:learnrate, weight_decay:weightdecay})
+            else:
+                 _, err, currentW, currentb, yhat = sess.run([train_adam, cross_entropy_loss_adam, w_adam, b_adam, y_predicted_adam], feed_dict={x_adam: minix, y_target_adam: miniy,learn_rate_adam:learnrate, weight_decay_adam:weightdecay})
         tempresult.append(err)
     epochs=(len(tempresult))
     result.append(tempresult)
     weight_list.append(currentW)
     bias_list.append(currentb)
-    #valid_err = sess.run(mse,feed_dict={x: validData, y_target: validTarget})
-    #print("Learning Rate " + str(rate) + ": " + str(valid_err))
-    #valid_mse_list.append(valid_err)
     sess.run(init)
+
+
+
 x = np.arange(epochs)
-y = result[0]
-color=['r','g','b']
-for idx, val in enumerate(learn):
-    line, = plt.plot(x, result[idx],color=color[idx], label="learning rate: "+str(val))
-    
+line, = plt.plot(x, result[0],color='b', label="Optimizer: SGD")
+line, = plt.plot(x, result[1],color='r', label="Optimizer: Adam Optimizer")
+
 plt.legend(loc='upper right', shadow=True, fontsize='x-large')
 
-    
 plt.ylabel('Training loss')
 plt.xlabel('Number of Epochs')
 plt.show()
