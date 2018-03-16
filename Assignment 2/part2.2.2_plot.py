8d993f542b351d33167155c99abb7005edaef682
ceciliawu <cecilia@ece.utoronto.ca>
2018-03-16 00:16:30 -0400

    part2.2.2 with plot
    part2.2.2 with plot

--- Assignment 2/part2.2.2_plot.py
+++ Assignment 2/part2.2.2_plot.py
@@ -59,55 +59,75 @@ num_batch_per_epoch = int(747/batch_size)
 num_epochs = int(iterations/num_batch_per_epoch)
 
 result=[]
-learn_rate=[0.005,0.001,0.0001]
-weight_d=[0.0,0.001,0.1,1]
+learn=0.001
+weight=0.1 #best learn rate and weight accuracy
 best_Y_Predicted=[]
-for learn in learn_rate:
-    for weight in weight_d:
-        tempresult=[]
-        start_index = 0
-        prev_count = 0
-        for i in range(0,iterations):
-            end_index = start_index + batch_size
-            if(end_index<747):
-                minix=trainFaceData[start_index:end_index]
-                miniy=one_hot_trainFaceTarget[start_index:end_index]
-                start_index = start_index + batch_size
-            else:
-                num_remaining = 747-start_index
-                minix[0:num_remaining] = trainFaceData[start_index:]
-                miniy[0:num_remaining] = one_hot_trainFaceTarget[start_index:]
-                num_still_need = batch_size-num_remaining
-                minix[num_remaining:batch_size] = trainFaceData[0:num_still_need]
-                miniy[num_remaining:batch_size] = one_hot_trainFaceTarget[0:num_still_need]
-                start_index = num_still_need
-            err,train_r,weight_w,bias,y_predicted=sess.run([loss,train,W,B,y_predicted_label],feed_dict={X:minix,y_target:miniy,learning_rate:learn,weight_decay:weight})
+valid_accuracy_result = []
+valid_error_result = []
+train_accuracy_result = []
+train_error_result = []
+tempresult = []
+start_index = 0
+prev_count = 0
+for i in range(0,iterations):
+    end_index = start_index + batch_size
+    if(end_index<747):
+        minix=trainFaceData[start_index:end_index]
+        miniy=one_hot_trainFaceTarget[start_index:end_index]
+        start_index = start_index + batch_size
+    else:
+        num_remaining = 747-start_index
+        minix[0:num_remaining] = trainFaceData[start_index:]
+        miniy[0:num_remaining] = one_hot_trainFaceTarget[start_index:]
+        num_still_need = batch_size-num_remaining
+        minix[num_remaining:batch_size] = trainFaceData[0:num_still_need]
+        miniy[num_remaining:batch_size] = one_hot_trainFaceTarget[0:num_still_need]
+        start_index = num_still_need
+    err,train_r,weight_w,bias,y_predicted=sess.run([loss,train,W,B,y_predicted_label],feed_dict={X:minix,y_target:miniy,learning_rate:learn,weight_decay:weight})
 
-            if((i*batch_size)/747 > prev_count):
-                prev_count = (i*batch_size)/747
-                tempresult.append(err)
+    if((i*batch_size)/747 > prev_count):
+        prev_count = (i*batch_size)/747
+        #training data accuracy and error calculation
+        y_predic =sess.run(tf.nn.softmax(sess.run(y_predicted_label,feed_dict={X:trainFaceData})))
+        prediction_accuracy=tf.equal(tf.argmax(y_predic,1),tf.argmax(one_hot_trainFaceTarget,1))
+        accur=sess.run(tf.reduce_mean(tf.cast(prediction_accuracy,tf.float32)))
+        train_accuracy_result.append(accur)
+        train_error_result.append(err)
 
+        #calcuate the accuracy of valid data for each epoch
         y_predic =sess.run(tf.nn.softmax(sess.run(y_predicted_label,feed_dict={X:validFaceData})))
         prediction_accuracy=tf.equal(tf.argmax(y_predic,1),tf.argmax(one_hot_validFaceTarget,1))
         accur=sess.run(tf.reduce_mean(tf.cast(prediction_accuracy,tf.float32)))
-        best_Y_Predicted.append(accur)
-        print("accuracy for learn rate "+str(learn)+" weight decay "+ str(weight) + ":   "+str(accur))
-        result.append(tempresult)
-        sess.run(init)
+        valid_accuracy_result.append(accur)
+        err,y_predicted=sess.run([loss,y_predicted_label],feed_dict={X:validFaceData,y_target:one_hot_validFaceTarget,weight_decay:weight})
+        valid_error_result.append(err)
 
-#epochs=(len(result[0]))
-#
-#x = np.arange(epochs)
-#
-#color=['r','g','b']
-#for idx, val in enumerate(learn_rate):
-#    line, = plt.plot(x, result[idx],color=color[idx], label="learning rate: "+str(val))
-#
-#plt.legend(loc='upper right', shadow=True, fontsize='x-large')
-#
-#plt.ylabel('Training loss')
-#plt.xlabel('Number of Epochs')
-#plt.show()
+y_predic =sess.run(tf.nn.softmax(sess.run(y_predicted_label,feed_dict={X:testFaceData})))
+prediction_accuracy=tf.equal(tf.argmax(y_predic,1),tf.argmax(one_hot_testFaceTarget,1))
+accur=sess.run(tf.reduce_mean(tf.cast(prediction_accuracy,tf.float32)))
+print("accuracy for test data is : " + str(accur))
+sess.run(init)
+
+x = np.arange(len(valid_accuracy_result))
+
+
+plt.interactive(False)
+# plot the cross entropy loss
+line_valid_error = plt.plot(x, valid_error_result,color='r', label="Valid Data Set Loss")
+line_test_accuracy = plt.plot(x, train_error_result,color='b', label="Train Data Set Loss")   
+plt.legend(loc='best', shadow=True, fontsize='small')    
+plt.ylabel('Cross Entropy Loss')
+plt.xlabel('Number of Epochs')
+plt.show()
+
+# plot the validation accuracy 
+line_valid_accuracy = plt.plot(x, valid_accuracy_result,color='r', label="Valid Data Set Accuracy")
+line_test_accuracy = plt.plot(x, train_accuracy_result,color='g', label="Test Data Set Accuracy")
+plt.legend(loc='best', shadow=True, fontsize='small')    
+plt.ylabel('Classification Accuracy')
+plt.xlabel('Number of Epochs')
+plt.show()
+
 
 
 
