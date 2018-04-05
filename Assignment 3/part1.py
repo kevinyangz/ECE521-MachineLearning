9bda5a8051de9356e2d0a4a661be6f0b27050582
kevinyangz <kevinze.yang@mail.utoronto.ca>
2018-04-05 01:18:19 -0400

    more clean version

--- Assignment 3/part1.py
+++ Assignment 3/part1.py
@@ -22,12 +22,12 @@ def load_data():
 def layer_block(input_tensor,n):
     
     shape=input_tensor.get_shape().as_list()[1]
-    print(input_tensor.shape[1].value)
+    #print(input_tensor.shape[1].value)
     initializer = tf.contrib.layers.xavier_initializer()
     W = tf.Variable(initializer([shape,n]),name='weights')
     b = tf.Variable(tf.zeros([1,n]), name='biases')
     output=tf.add(tf.matmul(input_tensor,W),b)
-    print(output.shape)
+    #print(output.shape)
     return W,b,output
 
 def build_graph():
@@ -35,14 +35,14 @@ def build_graph():
     X = tf.placeholder(tf.float32, [None, 28*28], name='input_x')
     y_target = tf.placeholder(tf.float32, [None,1], name='target_y')
     y_onehot = tf.one_hot(tf.to_int32(y_target), 10, 1.0, 0.0, axis = -1)
-
+    learn_rate=tf.placeholder(tf.float32,shape=[],name='learn_rate')
     W0,b0,output = layer_block(X,1000)
 
     W1,b1,relu_out= layer_block(tf.nn.relu(output),10)
-    relu_out=tf.transpose(relu_out)
+
     y_predicted=tf.nn.softmax(relu_out)
 
-    
+    #print(y_predicted.shape)
     crossEntropyError = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(\
     labels = y_onehot, logits = relu_out),name='mean_cross_entropy')
    
@@ -50,22 +50,24 @@ def build_graph():
     Wb=tf.reshape(W1,[1,-1])
 
     all_weight=tf.concat([Wn,Wb],1)
-    print(all_weight)
+
     weight_decay=tf.divide(0.0003,2)*tf.reduce_sum(all_weight*all_weight)
-    accuracy = tf.reduce_mean(tf.equal(tf.argmax(y_predicted,1),tf.to_int64(y_target))))
+    #test=tf.equal(tf.argmax(y_predicted,1),tf.to_int64(y_target))
+    #print(test.shape)
+    accuracy = 1-(tf.reduce_mean(tf.to_float(tf.equal(tf.reshape(tf.argmax(y_predicted,1),[-1,1]),tf.to_int64(y_target)))))
 
     loss = crossEntropyError+weight_decay 
     # Training mechanism
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate = 0.005)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate = learn_rate)
     train = optimizer.minimize(loss=loss)
     
-    return W0,b0,W1,b1,X, y_target, y_predicted, crossEntropyError, train, accuracy    
+    return W0,b0,W1,b1,X, y_target, y_predicted, crossEntropyError, train, accuracy,learn_rate    
 
 
 
 
 trainMinstData,trainMinstTarget,validMinstData,validMinstTarget,testMinstData,testMinstTarget=load_data()
-W0,b0,W1,b1,X,y_target, y_predicted_label, crossEntropyError, train, accuracy= build_graph()
+W0,b0,W1,b1,X,y_target, y_predicted_label, crossEntropyError, train, accuracy,learnrate= build_graph()
 init = tf.global_variables_initializer()
 sess = tf.InteractiveSession()
 sess.run(init)
@@ -89,7 +91,7 @@ for learn in learn_rate:
             start_index = i* 500
             minix=trainMinstData[start_index:start_index+500]
             miniy=trainMinstTarget[start_index:start_index+500]   
-            err,train_r,w0,bb0,w1,bb1,acc=sess.run([crossEntropyError,train,W0,b0,W1,b1,accuracy],feed_dict={X:minix,y_target:miniy})
+            err,train_r,w0,bb0,w1,bb1,acc=sess.run([crossEntropyError,train,W0,b0,W1,b1,accuracy],feed_dict={X:minix,y_target:miniy,learnrate:learn})
     	
         tempTrainresult.append(err)#Record Training loss each epoch
         tempTestacc.append(sess.run(accuracy,feed_dict={X:testMinstData,y_target:testMinstTarget})) #Test Acc
@@ -100,14 +102,19 @@ for learn in learn_rate:
     Trainresult.append(tempTrainresult)
     TrainAcc.append(tempTrainacc)
     TestAcc.append(tempTestacc)
-    ValidAcc.append(tempValidacc)
-			
+    ValidAcc.append(tempValidacc) 
+    		
 		
 	
     loss_result=sess.run(crossEntropyError,feed_dict={X:trainMinstData,y_target:trainMinstTarget})
     train_acc=sess.run([accuracy],feed_dict={X:trainMinstData,y_target:trainMinstTarget})
     test_acc=sess.run([accuracy],feed_dict={X:testMinstData,y_target:testMinstTarget})
-    valid_acc=sess.run([accuracy],feed_dict={X:validMinstData,y_target:validMinstTarget})
+    valid_acc,y_predicted=sess.run([accuracy,y_predicted_label],feed_dict={X:validMinstData,y_target:validMinstTarget})
+    test=tf.reduce_mean(tf.to_float(tf.equal(tf.reshape(tf.argmax(y_predicted,1),[-1,1]),tf.to_int64(validMinstTarget))))
+    
+
+    print(sess.run(test))
+    sess.run(init)
 
 
     print("learning rate: %s loss is %s"%(learn,loss_result)+" Train acc is %s"%train_acc +"Test acc is %s"%test_acc+"Valid acc is %s"%valid_acc)
@@ -117,14 +124,21 @@ color=['r','g','b']
 for idx,val in enumerate(learn_rate):
     #plt.figure(idx)
     plt.plot(x,Trainresult[idx],color=color[idx],label="learn rate %s"%val)
+    #plt.plot(x,TrainAcc[idx],color=color[0],label="Train data error")
+    #plt.plot(x,TestAcc[idx],color=color[1],label= "Test data error")
+    #plt.plot(x,ValidAcc[idx],color=color[2],label="Valid data error")
 
+    #plt.legend(loc='upper right',shadow=True,fontsize='x-large')
+    #plt.ylabel('cross entropy loss')
+    #plt.ylabel('error')
+    #plt.xlabel('Number of epoches')
+    #plt.title('Neural Network error vs number of epoches when learn rate '+str(val))
+    #plt.show()
 plt.legend(loc='upper right',shadow=True,fontsize='x-large')
-plt.ylabel('Training loss')
-plt.xlabel('Number of Epochs')
+plt.ylabel('cross entropy loss')
+plt.xlabel('Number of epoches')
+plt.title('Neural Network loss vs number of epoches')
 plt.show()
-
-
-
     #x=np.arange(num_epochs)
     #for idx,val in enumerate(learn_rate):
 	
