b0dbc5dad4012cf01e32d563b406834a66e5ccb3
kevinyangz <kevinze.yang@mail.utoronto.ca>
2018-04-05 16:27:01 -0400

    add random parameter

---
+++ Assignment 3/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+weight_decay_factor=np.random.uniform(-9,-6) #Random Natural Log for weight decay
+number_of_layer= np.int64(np.random.uniform(1,5.5)) #Random Natural Log for weight decay
+learn_rate=np.random.uniform(-7.5,-4.5) #Random Natural Log for weight decay
+dropout=np.random.choice([True, False])
+
