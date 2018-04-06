8b3d878502471aa3bfb31df976ebc55dafbd43f5
wusi9 <ceciliasiyi.wu@mail.utoronto.ca>
2018-04-05 20:44:55 -0400

    part1.3.1

--- Assignment 3/part1.3.1.py
+++ Assignment 3/part1.3.1.py
@@ -30,73 +30,68 @@ def layer_block(input_tensor,n):
 
     return W,b,output
 
-def build_graph():
+
+def build_graph(drop_out,weight_decay):
 
     X = tf.placeholder(tf.float32, [None, 28*28], name='input_x')
     y_target = tf.placeholder(tf.float32, [None,1], name='target_y')
     y_onehot = tf.one_hot(tf.to_int32(y_target), 10, 1.0, 0.0, axis = -1)
     learn_rate=tf.placeholder(tf.float32,shape=[],name='learn_rate')
-    apply_dropout = tf.placeholder(tf.bool,name='apply_dropout')
-    apply_weight_decay = tf.placeholder(tf.bool,name='apply_weight_decay')
     X_flatten=tf.reshape(X,[-1,28*28])
     W0,b0,output = layer_block(X_flatten,1000)
     activated_output = tf.nn.relu(output)   
-    #apply the drop out here
-    if (apply_dropout):
-	print("dropout applied.")
-	drop_out = tf.nn.dropout(activated_output, keep_prob =0.5)
-	W1,b1,final_output= layer_block(drop_out,10)
-    else: 
-	W1,b1,final_output= layer_block(activated_output,10)
+    dropout_output = []
+    #apply the drop out 
+    if(drop_out):
+       dropout_output = tf.nn.dropout(activated_output,keep_prob=0.5)
+       W1,b1,final_output= layer_block(dropout_output,10)
+    else:   
+       W1,b1,final_output= layer_block(activated_output,10)
 
-
-
-    #y_predicted=relu_out
-    #print(y_predicted.shape)
     crossEntropyError = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y_onehot, logits = final_output),name='mean_cross_entropy')
     y_predicted=tf.nn.softmax(final_output)
     Wn=tf.reshape(W0,[1,-1])
     Wb=tf.reshape(W1,[1,-1])
 
     all_weight=tf.concat([Wn,Wb],1)
-    if(apply_weight_decay):
-	print("weight decay applied")
-	weight_decay=tf.divide(0,2)*tf.reduce_sum(all_weight*all_weight)
+
+    if(weight_decay):
+        weight_decay = tf.divide(0,2)*tf.reduce_sum(all_weight*all_weight)
     else:
-	weight_decay = 0
-    #test=tf.equal(tf.argmax(y_predicted,1),tf.to_int64(y_target))
-    #print(test.shape)
+        weight_decay = 0
     accuracy = 1-(tf.reduce_mean(tf.to_float(tf.equal(tf.reshape(tf.argmax(y_predicted,1),[-1,1]),tf.to_int64(y_target)))))
 
-    loss = crossEntropyError + weight_decay
+    loss = crossEntropyError  + weight_decay
     # Training mechanism
     optimizer = tf.train.AdamOptimizer(learning_rate = learn_rate)
     train = optimizer.minimize(loss=loss)
     
-    return W0,b0,W1,b1,X, y_target, y_predicted, crossEntropyError, train, accuracy,learn_rate    
+    return W0,b0,W1,b1,X, y_target, y_predicted, crossEntropyError, train, accuracy,learn_rate,dropout_output,activated_output
 
 
 #parameters input
-apply_dropout = 1
-apply_weight_decay = 0
 
 trainMinstData,trainMinstTarget,validMinstData,validMinstTarget,testMinstData,testMinstTarget=load_data()
-W0,b0,W1,b1,X,y_target, y_predicted_label, crossEntropyError, train, accuracy,learnrate= build_graph()
 
 
-init = tf.global_variables_initializer()
-sess = tf.InteractiveSession()
-sess.run(init)
-iterations = 1000
+
+
+iterations = 10000
 num_epochs = int(iterations/30) #15000 Train Sample
 Trainresult=[]
 TrainAcc=[]
 TestAcc=[]
 ValidAcc=[]
 
-learn_rate=[0.001]
 
-for learn in learn_rate:
+learn=0.001
+dropout_list = [False, True]
+
+for droupout in dropout_list:
+    W0,b0,W1,b1,X,y_target, y_predicted_label, crossEntropyError, train, accuracy,learnrate,dropout_output,activated_output= build_graph(droupout,False)
+    init = tf.global_variables_initializer()
+    sess = tf.InteractiveSession()
+    sess.run(init)
     tempTrainresult=[]
     tempTrainacc=[]
     tempTestacc=[]
@@ -110,41 +105,24 @@ for learn in learn_rate:
             start_index = i* 500
             minix=trainMinstData[start_index:start_index+500]
             miniy=trainMinstTarget[start_index:start_index+500]   
-            err,train_r,w0,bb0,w1,bb1,acc=sess.run([crossEntropyError,train,W0,b0,W1,b1,accuracy],feed_dict={X:minix,y_target:miniy,learnrate:learn,apply_dropout:False,apply_weight_decay:False})
-	    err_d,train_r_d,w0_d,bb0_d,w1_d,bb1_d,acc_d=sess.run([crossEntropyError,train,W0,b0,W1,b1,accuracy],feed_dict={X:minix,y_target:miniy,learnrate:learn,apply_dropout:True,apply_weight_decay:False})
-    	
+            err,train_r,w0,bb0,w1,bb1,acc,dropout,activateout=sess.run([crossEntropyError,train,W0,b0,W1,b1,accuracy,dropout_output,activated_output],feed_dict={X:minix,y_target:miniy,learnrate:learn})
         tempTrainresult.append(err)#Record Training loss each epoch
         tempTestacc.append(sess.run(accuracy,feed_dict={X:testMinstData,y_target:testMinstTarget})) #Test Acc
         tempTrainacc.append(acc)#Train acc per mini-batch #Double check whether need the entire training set or not
         tempValidacc.append(sess.run(accuracy,feed_dict={X:validMinstData,y_target:validMinstTarget})) #Test Acc
-	#for drop out
-        tempTrainresult_d.append(err_d)#Record Training loss each epoch
-        tempTestacc_d.append(sess.run(accuracy_d,feed_dict={X:testMinstData,y_target:testMinstTarget})) #Test Acc
-        tempTrainacc_d.append(acc_d)#Train acc per mini-batch #Double check whether need the entire training set or not
-        tempValidacc.append(sess.run(accuracy_d,feed_dict={X:validMinstData,y_target:validMinstTarget})) #Test Acc
-        #print(str(step)+"-------"+str(bb0.shape)+"---"+str(bb1.shape))
 		
     Trainresult.append(tempTrainresult)
     TrainAcc.append(tempTrainacc)
     TestAcc.append(tempTestacc)
     ValidAcc.append(tempValidacc) 
 
-    Trainresult_d.append(tempTrainresult_d)
-    TrainAcc_d.append(tempTrainacc_d)
-    TestAcc_d.append(tempTestacc_d)
-    ValidAcc_d.append(tempValidacc_d)
-    		
 		
 	
     loss_result=sess.run(crossEntropyError,feed_dict={X:trainMinstData,y_target:trainMinstTarget})
     train_acc=sess.run([accuracy],feed_dict={X:trainMinstData,y_target:trainMinstTarget})
     test_acc=sess.run([accuracy],feed_dict={X:testMinstData,y_target:testMinstTarget})
     valid_acc,y_predicted=sess.run([accuracy,y_predicted_label],feed_dict={X:validMinstData,y_target:validMinstTarget})
-    #test=tf.reduce_mean(tf.to_float(tf.equal(tf.argmax(y_predicted,-1),tf.to_int64(validMinstTarget))))
-    
 
-    #print(sess.run(test))
-    sess.run(init)
 
 
     print("learning rate: %s loss is %s"%(learn,loss_result)+" Train error is %s"%train_acc +"Test error is %s"%test_acc+"Valid error is %s"%valid_acc)
@@ -173,20 +151,18 @@ if( not drawoption):
 	    #x=np.arange(num_epochs)
 	    #for idx,val in enumerate(learn_rate):
 if(drawoption):
-    
-	x=np.arange(num_epochs)
-	color=['r','g','b','yellow','teal','violet']
-	for idx,val in enumerate(learn_rate):
-	    plt.figure(idx)
+    x=np.arange(num_epochs)
+    color=['r','g','b','yellow','teal','violet']
+	#plt.figure(idx)
+    plt.plot(x,TrainAcc[0],color=color[0],label="Train data error without Dropout")
+    plt.plot(x,TrainAcc[1],color=color[1],label="Train data error with Dropout")
+    plt.plot(x,ValidAcc[0],color=color[2],label="Valid data error without Dropout")
+    plt.plot(x,ValidAcc[1],color=color[3],label="Valid data error with Dropout")
 
-	    plt.plot(x,TrainAcc[idx],color=color[0],label="Train data error")
-	    #plt.plot(x,TestAcc[idx],color=color[1],label= "Test data error")
-	    plt.plot(x,ValidAcc[idx],color=color[2],label="Valid data error")
+    plt.legend(loc='best',shadow=True,fontsize='x-large')
 
-	    plt.legend(loc='best',shadow=True,fontsize='x-large')
-
-	    plt.ylabel('error')
-	    plt.xlabel('Number of epochs')
-	    plt.title('Neural Network Error vs Number of Epochs with Learn Rate '+str(val))
-	    plt.show()
+    plt.ylabel('error')
+    plt.xlabel('Number of epochs')
+    plt.title('Neural Network Error vs Number of Epochs with/without Dropout')
+    plt.show()
 	
